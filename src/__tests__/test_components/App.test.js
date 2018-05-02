4949f6a6cf23587a759c6853a136fb6f26772ab0
jose <jennymarin1989@hotmail.com>
2018-05-02 12:44:37 +0100

    :recycle: Refactor of test in App.js, before each was applied

--- src/__tests__/test_components/App.test.js
+++ src/__tests__/test_components/App.test.js
@@ -5,20 +5,24 @@ import App from '../../components/App.js'
 
 describe('App', () =>{
 
-  let app = shallow(<App />)
+  let app;
+
+  beforeEach(() => {
+    app = shallow(<App />)
+  });
+
+
   it("initializes correctly", () =>{
     expect(app).toMatchSnapshot();
   });
 
   describe('initializes state successfully', ()=> {
-    let app = shallow(<App />)
     it("initializes state as an empty string", () => {
       expect(app.state('input')).toEqual("");
     });
   })
 
   describe('renders input correctly', () => {
-    let app = shallow(<App />)
     it('renders year input correctly', () => {
       expect(app.find('#year-input').length).toEqual(1);
     })

8ee3119cbd3eb37aba1b2f3067e3646add4b4273
jose <jennymarin1989@hotmail.com>
2018-05-02 09:00:46 +0100

    :white_check_mark: Pass test for initiliazing state of empty input

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,16 @@ import React, { Component} from 'react';
 
 class App extends Component {
     
+  constructor(){
+    super();
+    this.state = {
+      input: ""
+    }
+
+  }
+
+
+
   render(){  
     return (
       <h1>REACT DJ</h1>

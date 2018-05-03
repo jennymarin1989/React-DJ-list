b64fe27c00bd367fd0986101757b8105ca62a87d
jose <jennymarin1989@hotmail.com>
2018-05-03 02:26:10 +0100

    :Removing code or files:Remove SubmitButton - For MVP is not needed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component} from 'react';
 import ListOfSongs from './ListOfSongs';
 import data from '../db.json'
-import SubmitButton from './SubmitButton';
+
 
 
 class App extends Component {
@@ -25,15 +25,12 @@ class App extends Component {
       return song.Year === parseInt(this.state.event)
     }
   )
-  
+
     return (
       <div>
         <h1>REACT DJ</h1>
         <div className="input-group">
           <input type="text" id="year-input" className="form-control" name="input" placeholder="Enter a year" value={this.state.message} onChange={this.handleInputChange}/>
-          <SubmitButton
-            name={"Search"}
-             />
         </div>
         <div>
           <ListOfSongs 
